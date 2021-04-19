b0cc83e670ace79b3b71246bb62a8effc97bdf44
IgorEulalio <igoreulalio.ie@gmail.com>
2021-04-19 16:35:51 -0300

    ordering for performance test purposes

--- src/main/java/com/igoreul/example/GreetingsController.java
+++ src/main/java/com/igoreul/example/GreetingsController.java
@@ -2,11 +2,64 @@ package com.igoreul.example;
 
 import io.micronaut.http.annotation.*;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Controller("/greetings")
 public class GreetingsController {
 
+    private final List<String> FullList= Arrays.asList("aabcde", "aabchu",
+            "uuickw", "uuhisoa",
+            "uuisz3", "uuisk7",
+            "aabloe", "uuhkol",
+            "cccd", "cccc", "cccd9", "cccc",
+            "uuhbx", "uuibxt9", "uuilok", "uuizxed");
+
     @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    public List<String> index() {
+
+        return returnTopRepeated(FullList, 2);
+    }
+
+    public List<String> returnTopRepeated(List<String> receiveStrings, Integer numbersOfElements){
+
+        Map<String, Integer> mapeamentoDeStrings = new TreeMap<>();
+
+        receiveStrings.forEach(
+                str -> {
+                    var first3Chars = str.substring(0, 3);
+                    System.out.println(first3Chars);
+                    if(mapeamentoDeStrings.containsKey(first3Chars)){
+                        Integer indexOfThisString = mapeamentoDeStrings.get(first3Chars);
+                        var newIndex = indexOfThisString + 1;
+                        mapeamentoDeStrings.put(first3Chars, newIndex);
+                    }
+                    else {
+                        mapeamentoDeStrings.put(first3Chars, 1);
+                    }
+                }
+        );
+
+        Map<String, Integer> orderedMapByValues = sortByValue(mapeamentoDeStrings,false, numbersOfElements);
+        System.out.println(orderedMapByValues);
+        Set<String> strings = orderedMapByValues.keySet();
+        List<String> collect = strings.stream().collect(Collectors.toList());
+        return collect;
+    }
+
+    private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap, final boolean order,Integer elementsToReturn)
+    {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
+                ? o1.getKey().compareTo(o2.getKey())
+                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
+                ? o2.getKey().compareTo(o1.getKey())
+                : o2.getValue().compareTo(o1.getValue()));
+
+        return list.stream()
+                .limit(elementsToReturn)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
     }
 }
